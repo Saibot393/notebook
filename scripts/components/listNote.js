eb12d0bb12a1b128b1a102e410e37f3a1bd2faa9
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2024-05-28 19:44:52 +0200

    some list improvements

--- scripts/components/listNote.js
+++ scripts/components/listNote.js
@@ -7,6 +7,8 @@ import {registerHoverShadow} from "../helpers/visualHelpers.js";
 
 const cCheckIcon = "fa-check";
 
+const cMaxIndent = 5;
+
 const cIndent = "25"; //px
 
 export class listNote extends basicNote {
@@ -41,15 +43,30 @@ export class listNote extends basicNote {
 	get JournalPage() {
 		let vText;
 		
+		let vPrevIndent = 0;
+		
 		vText = "<ul>";
 		
 		for (let vEntry of this.list) {
+			for (let i = vPrevIndent; i > vEntry.indent; i--) {
+				vText = vText + "</ul>";
+			}
+			for (let i = vPrevIndent; i < vEntry.indent; i++) {
+				vText = vText + "<ul>";
+			}
+			
 			vText = vText + "<li>";
 			vText = vText + vEntry.text;
 			//vText = vText + `<i class"fa-solid ${cCheckIcon}">`;
 			vText = vText + "</li>";
+			
+			vPrevIndent = vEntry.indent || 0;
 		}
 		
+		for (let i = vPrevIndent; i > 0; i--) {
+			vText = vText + "</ul>";
+		}
+	
 		vText = vText + "</ul>";
 		
 		return {
@@ -104,12 +121,108 @@ export class listNote extends basicNote {
 		}
 	}
 	
+	toggleWithIndents(pID) {
+		let vList = this.list;
+		
+		if (pID < vList.length) {
+			vList[pID].checked = !vList[pID].checked;
+			let vCheckedTarget = vList[pID].checked;
+			let vIndentLimit = vList[pID].indent || 0;
+			let i = pID + 1;
+			
+			while (i < vList.length && (vList[i].indent || 0) > vIndentLimit) {
+				vList[i].checked = vCheckedTarget;
+				i = i + 1;
+			}
+			
+			this.list = vList;
+		}
+	}
+	
+	changeIndent(pID, pIndent) {
+		if (pIndent) {
+			let vList = this.list;
+			
+			if (pID < vList.length && pID > 0) {
+				let vPrevIndent = vList[pID-1].indent || 0;
+				let vTarget = Math.max(0, Math.min(Math.min(cMaxIndent, vPrevIndent+1), vList[pID].indent + pIndent));
+
+				if (vList[pID].indent != vTarget) {
+					let vChange = vTarget - vList[pID].indent;
+					
+					let i = pID + 1;
+					while (i < vList.length && (vList[i].indent || 0) > vList[pID].indent) {
+						if (vChange < 0 || vList[i].indent < cMaxIndent) {
+							vList[i].indent = vList[i].indent + vChange;
+						}
+						
+						i = i + 1;
+					}
+					
+					vList[pID].indent = vTarget;
+					this.list = vList;
+				}
+			}
+			else {
+				if (pID == 0 && vList[0].indent != 0) {
+					vList[0].indent = 0;
+					
+					this.list = vList;
+				}
+			}
+		}
+	}
+	
 	changeText(pID, pText) {
 		let vList = this.list;
 		
 		if (pID < vList.length && vList[pID].text != pText) {
 			vList[pID].text = pText;
 			this.list = vList;
+		}
+	}
+	
+	insert(pSource, pPosition) {
+		let vList = this.list;
+		
+		let vPosition = Math.max(0, Math.min(vList.length-1, pPosition));
+		
+		if (pSource != vPosition && pSource < vList.length && pSource > 0) {
+			let vInsert = [pSource];
+			
+			let vIndentLimit = vList[pSource].indent;
+			
+			let i = pSource + 1;
+			
+			while (i < vList.length && vList[i].indent > vIndentLimit) {
+				i = i + 1;
+			}
+			
+			vInsert.push(i - 1);
+			
+			let vCopyLength = vInsert[1] - vInsert[0] + 1;
+			
+			if ((vPosition < vInsert[0] || vPosition > vInsert[1]) && vCopyLength > 0) {
+				if (vPosition > vInsert[1]) {
+					vPosition = vPosition - (vCopyLength);
+				}
+				
+				let vInsertList = vList.splice(vInsert[0], vCopyLength);
+				
+				let vPrevIdent = vPosition == 0 ? -1 : vList[vPosition-1]?.indent || 0;
+				
+				let vIndentDiff = vInsertList[0].indent - vPrevIdent;
+				
+				if (vIndentDiff > 1) {
+					for (let vEntry of vInsertList) {
+						vEntry.indent = vEntry.indent - (vIndentDiff - 1);
+					}
+				}
+				
+				vList = [...vList.slice(0, vPosition), ...vInsertList, ...vList.slice(vPosition, vList.length)];
+				
+				this.updateContent({list : vList}, {position : vPosition, inserts : vInsert});
+			}
 		}
 	}
 	
@@ -158,6 +271,7 @@ export class listNote extends basicNote {
 				let vEntry = document.createElement("div");
 				vEntry.style.display = "flex";
 				vEntry.style.marginBottom = "3px";
+				vEntry.draggable = true;
 				
 				let vCheckBorder = document.createElement("div");
 				vCheckBorder.style.margin = "auto";
@@ -173,7 +287,7 @@ export class listNote extends basicNote {
 				vCheck.classList.add("fa-solid", cCheckIcon);
 				vCheck.style.color = this.primeColor;
 				registerHoverShadow(vCheck);
-				vSetCheckState(vCheck, vList[i].checked ?? false);
+				//vSetCheckState(vCheck, vList[i].checked ?? false);
 				vCheckBorder.appendChild(vCheck);
 				vCheckBorder.onclick = () => {
 					if (this.canEdit) {
@@ -181,10 +295,13 @@ export class listNote extends basicNote {
 						this.changeCheck(i, !vCheckState(vCheck));
 					}
 				}
+				vCheckBorder.oncontextmenu = () => {
+					this.toggleWithIndents(i);
+				}
 				
 				let vText = document.createElement("input");
 				vText.type = "text";
-				vText.value = vList[i].text || "";
+				//vText.value = vList[i].text || "";
 				vText.style.borderRadius = "0";
 				vText.style.border = "0px";
 				vText.oninput = () => {
@@ -199,8 +316,14 @@ export class listNote extends basicNote {
 							this.contentElements.listElements[(i+1)%this.contentElements.listElements.length].text.select();
 							break;
 						case "Tab" :
-							console.log("indentions comming soon");
-							//pEvent.preventDefault();
+							pEvent.preventDefault();
+							
+							if (pEvent.shiftKey) {
+								this.changeIndent(i, -1);
+							}
+							else {
+								this.changeIndent(i, 1);
+							}
 							break;
 					}		
 				}
@@ -232,20 +355,24 @@ export class listNote extends basicNote {
 					delete : vDelete
 				}
 			}
-			else {
-				let vElement = this.contentElements.listElements[i];
+
+			let vElement = this.contentElements.listElements[i];
+			
+			let vIndent = (vList[i].indent || 0) * cIndent + "px";
+			if (vElement.div.style.paddingLeft != vIndent) {
+				vElement.div.style.paddingLeft = vIndent;
+			}
+			
+			let vChecked = vList[i].checked;
+			if (vCheckState(vElement.check) != vChecked) {
+				vNotify = true;
 				
-				let vChecked = vList[i].checked;
-				if (vCheckState(vElement.check) != vChecked) {
-					vNotify = true;
-					
-					vSetCheckState(vElement.check, vChecked);
-				}
-				
-				let vText = vList[i].text || "";
-				if (vElement.text.value != vText) {
-					vElement.text.value = vText;
-				}
+				vSetCheckState(vElement.check, vChecked);
+			}
+			
+			let vText = vList[i].text || "";
+			if (vElement.text.value != vText) {
+				vElement.text.value = vText;
 			}
 		}
 		
